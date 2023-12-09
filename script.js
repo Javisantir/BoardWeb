495b1ba4b24ab183fe57fcd289b04c51de885181
Javier Sánchez Tirados <78958304+Javisantir@users.noreply.github.com>
2023-12-09 18:34:34 +0100

    Functional with photos

---
+++ script.js
@@ -0,0 +1,58 @@
+const canvas = document.getElementById('pizarra');
+const ctx = canvas.getContext('2d');
+const fileInput = document.getElementById('fileInput');
+
+function resizeCanvas() {
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+}
+
+window.addEventListener('resize', resizeCanvas);
+resizeCanvas();
+
+let painting = false;
+
+function startPosition(e) {
+    painting = true;
+    draw(e);
+}
+
+function finishedPosition() {
+    painting = false;
+    ctx.beginPath();
+}
+
+function draw(e) {
+    if (!painting) return;
+    ctx.lineWidth = 5;
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = 'black';
+
+    const rect = canvas.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+}
+
+canvas.addEventListener('mousedown', startPosition);
+canvas.addEventListener('mouseup', finishedPosition);
+canvas.addEventListener('mousemove', draw);
+
+fileInput.addEventListener('change', function(e) {
+    if (e.target.files && e.target.files[0]) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const img = new Image();
+            img.onload = function() {
+                resizeCanvas();
+                ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+            }
+            img.src = e.target.result;
+        }
+        reader.readAsDataURL(e.target.files[0]);
+    }
+});
