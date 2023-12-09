ecced65052407e4123a031e543748e914dcc7248
Javier Sánchez Tirados <78958304+Javisantir@users.noreply.github.com>
2023-12-09 23:09:31 +0100

    V3 - Added Icons

--- script.js
+++ script.js
@@ -2,33 +2,78 @@ const drawingCanvas = document.getElementById('Board');
 const imageCanvas = document.getElementById('imageCanvas');
 const drawingCtx = drawingCanvas.getContext('2d');
 const imageCtx = imageCanvas.getContext('2d');
-const fileInput = document.getElementById('fileInput');
 const colorPicker = document.getElementById('colorPicker');
 const eraserBtn = document.getElementById('EraserBtn');
 const clearBtn = document.getElementById('clearBtn');
 const brushSizeSlider = document.getElementById('brushSize');
-
+const fileSelector = document.getElementById('fileSelector');
+const icons = document.querySelectorAll('.icon');
+let selectedIcon = null;
+let currentImageSrc = '';
 let painting = false;
 let isErasing = false;
 
-function resizeCanvas() {
-    drawingCanvas.width = imageCanvas.offsetWidth;
-    drawingCanvas.height = imageCanvas.offsetHeight;
-}
+document.querySelectorAll('.icon').forEach(icon => {
+    icon.addEventListener('click', function() {
+        selectedIcon = this.getAttribute('data-icon');
+        if (this.classList.contains('selected')) {
+            this.classList.remove('selected');
+            selectedIcon = null; // Elimina la selección si se hace clic nuevamente en el mismo icono
+        } else {
+            icons.forEach(icon => {
+                icon.classList.remove('selected');
+            });
+            this.classList.add('selected');
+            selectedIcon = this.getAttribute('data-icon');
+        }
+    });
+});
 
-window.addEventListener('resize', resizeCanvas);
-resizeCanvas();
+// Agrega el código JavaScript anterior aquí
+
+
+drawingCanvas.addEventListener('click', function(event) {
+    if (selectedIcon) {
+        const rect = drawingCanvas.getBoundingClientRect();
+        const scaleX = drawingCanvas.width / rect.width;
+        const scaleY = drawingCanvas.height / rect.height;
+        const x = (event.clientX - rect.left) * scaleX;
+        const y = (event.clientY - rect.top) * scaleY;
+
+        const img = new Image();
+        img.onload = function() {
+            // Ajustar las coordenadas x, y para que el icono se coloque en el lugar correcto
+            drawingCtx.drawImage(img, x - img.width / 2, y - img.height / 2, 50, 50);
+        };
+        img.src = selectedIcon;
+    }
+});
+
+
+fileSelector.addEventListener('change', function() {
+    const selectedFile = fileSelector.value;
+    currentImageSrc = '/media/maps/' + selectedFile;
+    const img = new Image();
+    img.onload = function() {
+        const newSize = adjustImageSize(img.width, img.height, window.innerWidth, window.innerHeight);
+        imageCanvas.width = newSize.width;
+        imageCanvas.height = newSize.height;
+        drawingCanvas.width = newSize.width;
+        drawingCanvas.height = newSize.height;
+        imageCtx.drawImage(img, 0, 0, newSize.width, newSize.height);
+    };
+    img.src = currentImageSrc;
+});
 
 eraserBtn.addEventListener('click', function() {
     isErasing = !isErasing;
-
     if (isErasing) {
         drawingCtx.globalCompositeOperation = 'destination-out';
-        eraserBtn.classList.add('active'); // Añadir una clase cuando el borrador está activo
+        eraserBtn.classList.add('active');
     } else {
         drawingCtx.globalCompositeOperation = 'source-over';
         drawingCtx.strokeStyle = colorPicker.value;
-        eraserBtn.classList.remove('active'); // Remover la clase cuando el borrador no está activo
+        eraserBtn.classList.remove('active');
     }
 });
 
@@ -52,19 +97,16 @@ function finishedPosition() {
 
 function draw(e) {
     if (!painting) return;
-
     drawingCtx.lineCap = 'round';
-
+    drawingCtx.lineWidth = isErasing ? brushSizeSlider.value : brushSizeSlider.value;
     if (!isErasing) {
         drawingCtx.strokeStyle = colorPicker.value;
     }
-
     const rect = drawingCanvas.getBoundingClientRect();
     const scaleX = drawingCanvas.width / rect.width;
     const scaleY = drawingCanvas.height / rect.height;
     const x = (e.clientX - rect.left) * scaleX;
     const y = (e.clientY - rect.top) * scaleY;
-
     drawingCtx.lineTo(x, y);
     drawingCtx.stroke();
     drawingCtx.beginPath();
@@ -78,34 +120,10 @@ drawingCanvas.addEventListener('mousemove', draw);
 function adjustImageSize(imgWidth, imgHeight, maxWidth, maxHeight) {
     let newWidth = imgWidth;
     let newHeight = imgHeight;
-
     if (imgWidth > maxWidth || imgHeight > maxHeight) {
         const ratio = Math.min(maxWidth / imgWidth, maxHeight / imgHeight);
         newWidth = imgWidth * ratio;
         newHeight = imgHeight * ratio;
     }
-
     return { width: newWidth, height: newHeight };
 }
-
-fileInput.addEventListener('change', function(e) {
-    if (e.target.files && e.target.files[0]) {
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            const img = new Image();
-            img.onload = function() {
-                const maxSize = { width: window.innerWidth, height: window.innerHeight };
-                const newSize = adjustImageSize(img.width, img.height, maxSize.width, maxSize.height);
-                
-                imageCanvas.width = newSize.width;
-                imageCanvas.height = newSize.height;
-                drawingCanvas.width = newSize.width;
-                drawingCanvas.height = newSize.height;
-
-                imageCtx.drawImage(img, 0, 0, newSize.width, newSize.height);
-            };
-            img.src = e.target.result;
-        };
-        reader.readAsDataURL(e.target.files[0]);
-    }
-});
