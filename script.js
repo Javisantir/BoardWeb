09e31727671577f6180f843d43e4d21cad5bb3cd
Javier Sánchez Tirados <78958304+Javisantir@users.noreply.github.com>
2023-12-10 01:13:47 +0100

    V4 - TEXT

--- script.js
+++ script.js
@@ -7,20 +7,24 @@ const eraserBtn = document.getElementById('EraserBtn');
 const clearBtn = document.getElementById('clearBtn');
 const brushSizeSlider = document.getElementById('brushSize');
 const fileSelector = document.getElementById('fileSelector');
-const iconSizeSlider = document.getElementById('iconSize'); // Nuevo control deslizante para el tamaño del icono
+const iconSizeSlider = document.getElementById('iconSize');
+const textInput = document.getElementById('textInput');
+const submitTextBtn = document.getElementById('submitText');
+const textIconsContainer = document.getElementById('text-icons-container');
+const textSizeSlider = document.getElementById('textSizeSlider');
 
+let textSize = 16; // Valor inicial del tamaño del texto
 let selectedIcon = null;
+let selectedText = null;
 let currentImageSrc = '';
 let painting = false;
 let isErasing = false;
 let iconSize = 50; // Valor inicial para el tamaño del icono
 
-// Evento para cambiar el tamaño del icono
 iconSizeSlider.addEventListener('input', function() {
     iconSize = this.value;
 });
 
-// Selección de iconos
 document.querySelectorAll('.icon').forEach(icon => {
     icon.addEventListener('click', function() {
         if (this.classList.contains('selected')) {
@@ -35,57 +39,28 @@ document.querySelectorAll('.icon').forEach(icon => {
     });
 });
 
-// Dibujar icono en el lienzo
-drawingCanvas.addEventListener('click', function(event) {
-    if (selectedIcon) {
-        const rect = drawingCanvas.getBoundingClientRect();
-        const scaleX = drawingCanvas.width / rect.width;
-        const scaleY = drawingCanvas.height / rect.height;
-        const x = (event.clientX - rect.left) * scaleX;
-        const y = (event.clientY - rect.top) * scaleY;
+submitTextBtn.addEventListener('click', function() {
+    const words = textInput.value.split(' ');
+    textIconsContainer.innerHTML = '';
 
-        const img = new Image();
-        img.onload = function() {
-            drawingCtx.drawImage(img, x - iconSize / 2, y - iconSize / 2, iconSize, iconSize);
-        };
-        img.src = selectedIcon;
-    }
+    words.forEach(word => {
+        if (word.trim() !== '') {
+            const textIcon = document.createElement('div');
+            textIcon.classList.add('text-icon');
+            textIcon.textContent = word;
+            textIcon.addEventListener('click', function() {
+                document.querySelectorAll('.icon, .text-icon').forEach(icon => icon.classList.remove('selected'));
+                selectedIcon = null;
+
+                this.classList.add('selected');
+                selectedText = word;
+            });
+            textIconsContainer.appendChild(textIcon);
+        }
+    });
 });
 
-// Cargar imagen de fondo
-fileSelector.addEventListener('change', function() {
-    const selectedFile = fileSelector.value;
-    currentImageSrc = 'media/maps/' + selectedFile;
-    const img = new Image();
-    img.onload = function() {
-        const newSize = adjustImageSize(img.width, img.height, window.innerWidth, window.innerHeight);
-        imageCanvas.width = newSize.width;
-        imageCanvas.height = newSize.height;
-        drawingCanvas.width = newSize.width;
-        drawingCanvas.height = newSize.height;
-        imageCtx.drawImage(img, 0, 0, newSize.width, newSize.height);
-    };
-    img.src = currentImageSrc;
-});
 
-// Funcionalidad del borrador
-eraserBtn.addEventListener('click', function() {
-    isErasing = !isErasing;
-    drawingCtx.globalCompositeOperation = isErasing ? 'destination-out' : 'source-over';
-    eraserBtn.classList.toggle('active', isErasing);
-});
-
-// Ajustar tamaño del pincel/borrador
-brushSizeSlider.addEventListener('input', function() {
-    drawingCtx.lineWidth = brushSizeSlider.value;
-});
-
-// Limpiar el lienzo
-clearBtn.addEventListener('click', function() {
-    drawingCtx.clearRect(0, 0, drawingCanvas.width, drawingCanvas.height);
-});
-
-// Funciones para el dibujo
 function startPosition(e) {
     painting = true;
     draw(e);
@@ -116,7 +91,65 @@ drawingCanvas.addEventListener('mousedown', startPosition);
 drawingCanvas.addEventListener('mouseup', finishedPosition);
 drawingCanvas.addEventListener('mousemove', draw);
 
-// Ajustar tamaño de la imagen de fondo
+// Funcionalidad para cargar imágenes de fondo
+fileSelector.addEventListener('change', function() {
+    const selectedFile = fileSelector.value;
+    currentImageSrc = 'media/maps/' + selectedFile;
+    
+    const img = new Image();
+    img.onload = function() {
+        const newSize = adjustImageSize(img.width, img.height, window.innerWidth, window.innerHeight);
+        imageCanvas.width = newSize.width;
+        imageCanvas.height = newSize.height;
+        drawingCanvas.width = newSize.width;
+        drawingCanvas.height = newSize.height;
+        imageCtx.drawImage(img, 0, 0, newSize.width, newSize.height);
+    };
+    img.src = currentImageSrc;
+});
+
+// Funcionalidades de borrador y limpieza
+eraserBtn.addEventListener('click', function() {
+    isErasing = !isErasing;
+    if (isErasing) {
+        drawingCtx.globalCompositeOperation = 'destination-out';
+        eraserBtn.classList.add('active');
+        colorPicker.classList.add('inactive'); // Añadir clase al input de color
+    } else {
+        drawingCtx.globalCompositeOperation = 'source-over';
+        eraserBtn.classList.remove('active');
+        colorPicker.classList.remove('inactive'); // Quitar clase del input de color
+    }
+});
+
+function draw(e) {
+    if (!painting) return;
+    drawingCtx.lineCap = 'round';
+    drawingCtx.lineWidth = brushSizeSlider.value;
+
+    // La siguiente línea podría no ser necesaria si estás utilizando 'globalCompositeOperation'
+    // drawingCtx.strokeStyle = isErasing ? 'rgba(0,0,0,0)' : colorPicker.value;
+
+    const rect = drawingCanvas.getBoundingClientRect();
+    const scaleX = drawingCanvas.width / rect.width;
+    const scaleY = drawingCanvas.height / rect.height;
+    const x = (e.clientX - rect.left) * scaleX;
+    const y = (e.clientY - rect.top) * scaleY;
+    drawingCtx.lineTo(x, y);
+    drawingCtx.stroke();
+    drawingCtx.beginPath();
+    drawingCtx.moveTo(x, y);
+}
+
+brushSizeSlider.addEventListener('input', function() {
+    drawingCtx.lineWidth = brushSizeSlider.value;
+});
+
+clearBtn.addEventListener('click', function() {
+    drawingCtx.clearRect(0, 0, drawingCanvas.width, drawingCanvas.height);
+});
+
+// Función para ajustar el tamaño de la imagen
 function adjustImageSize(imgWidth, imgHeight, maxWidth, maxHeight) {
     let newWidth = imgWidth;
     let newHeight = imgHeight;
@@ -127,3 +160,40 @@ function adjustImageSize(imgWidth, imgHeight, maxWidth, maxHeight) {
     }
     return { width: newWidth, height: newHeight };
 }
+
+textSizeSlider.addEventListener('input', function() {
+    textSize = this.value;
+});
+
+textSizeSlider.addEventListener('input', function() {
+    textSize = this.value;
+});
+
+// Funcionalidad para dibujar texto e iconos en el lienzo
+drawingCanvas.addEventListener('click', function(event) {
+    const rect = drawingCanvas.getBoundingClientRect();
+    const scaleX = drawingCanvas.width / rect.width;
+    const scaleY = drawingCanvas.height / rect.height;
+    const x = (event.clientX - rect.left) * scaleX;
+    const y = (event.clientY - rect.top) * scaleY;
+
+    if (selectedIcon) {
+        const img = new Image();
+        img.onload = function() {
+            drawingCtx.drawImage(img, x - iconSize / 2, y - iconSize / 2, iconSize, iconSize);
+        };
+        img.src = selectedIcon;
+    } else if (selectedText) {
+        drawingCtx.font = textSize + "px Arial";
+        const textWidth = drawingCtx.measureText(selectedText).width;
+        const textHeight = parseInt(textSize, 10) * 1.2;
+
+        // Dibujar un rectángulo blanco detrás del texto
+        drawingCtx.fillStyle = 'white';
+        drawingCtx.fillRect(x - 5, y - parseInt(textSize, 10), textWidth + 10, textHeight);
+
+        // Dibujar el texto
+        drawingCtx.fillStyle = 'black';
+        drawingCtx.fillText(selectedText, x, y);
+    }
+});
